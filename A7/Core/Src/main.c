e827436810321f5ec240bf43e640e14f64bd0f57
rah1236 <39041279+rah1236@users.noreply.github.com>
2024-05-08 20:23:15 -0700

    Submitted Lab A7

--- A7/Core/Src/main.c
+++ A7/Core/Src/main.c
@@ -1,3 +1,30 @@
+/* USER CODE BEGIN Header */
+/*******************************************************************************
+ * EE 329 A7: UART
+ *******************************************************************************
+ * @file           : main.c
+ * @brief          : This is a small game like interface that
+ * 					 streams over uart to your terminal
+ * project         : EE 329 S'24 Assignment 7
+ * authors         : Raheel Rehmatullah and Harrison Vinas
+ * version         : 1
+ * date            : 5_8_2024
+ * compiler        : STM32CubeIDE v.1.15.0
+ * target          : NUCLEO-L4A6ZG
+ * clocks          : 4 MHz MSI to AHB2
+ * @attention      : (c) 2023 STMicroelectronics.  All rights reserved.
+ *******************************************************************************
+ * Pin Map:
+ *
+ * PG7 - UART TX
+ * PG8 - UART RX
+ *
+ *******************************************************************************
+ *
+ * UART Code credit to Braydon Burkhardt May 23, 2023
+ *
+ *******************************************************************************/
+/* USER CODE END Header */
 
 #include "main.h"
 #include "uart.h"
@@ -11,57 +38,130 @@ extern char keyPressed;
 int main(void)
 {
 
-  char splash = 'Ω';
   HAL_Init();
   SystemClock_Config();
+  //Init UART and clear the screen
   LPUART_init();
   LPUART_clearScreen();
 
-  int playerLocation[2] = {0, 12};
+  // Player location array, where [0] is on the X axis, and [1] is on the Y axis
+  // Positive is going down and to the right
+  int8_t player_location[2] = {40, 12};
+
+  //Defining Display buffer
+  char display_buffer[24][81];
+
+  //For loop for the splash screen
+  //Does a funky little animation for a few seconds
+  for (int i; i < 10; i++){
+
+	  for (int row = 0; row < 24; row++){
+		  if(row < 10){
+			strcpy(display_buffer[row],
+					"////////////////////----------------------------------------||||||||||||||||||||\0");
+		  }
+		  else if (row > 10 && row < 12){
+			strcpy(display_buffer[row],
+					"----------------------------- Mike's Floating Eye ------------------------------\0");
+		  }
+		  else{
+			strcpy(display_buffer[row],
+					"||||||||||||||||||||----------------------------------------////////////////////\0");
+		  }
+
+	  }
+
+	  refresh_screen(display_buffer);
 
 
-  char defaultString[80] =
-		  "AAAAAAAAAABBBBBBBBBBAAAAAAAAAABBBBBBBBBBAAAAAAAAAABBBBBBBBBBAAAAAAAAAABBBBBBBBB\0";
+	  for (int row = 0; row < 24; row++){
+		  if(row < 10){
+			strcpy(display_buffer[row],
+					"||||||||||||||||||||----------------------------------------////////////////////\0");
+		  }
+		  else if (row > 10 && row < 12){
+			strcpy(display_buffer[row],
+					"----------------------------- Mike's Floating Eye ------------------------------\0");
+		  }
+		  else{
+			strcpy(display_buffer[row],
+					"////////////////////----------------------------------------||||||||||||||||||||\0");
+		  }
 
+	  }
 
-  char displayBuffer[24][81];
-
-  for (int row = 0; row < 24; row++){
-		strcpy(displayBuffer[row],"////////////////////----------------------------------------\\\\\\\\\\\\\\\\\\\\\0");
+	  refresh_screen(display_buffer);
   }
-
-
-
   int refreshing = 1;
-  int row = 0;
 
-
+  //Starting infinite loop
   while (1)
   {
+
+	  //Defining border
+	  for (int row = 0; row < 24; row++){
+	  		strcpy(display_buffer[row],
+	  				"                                                                                \0");
+	  		display_buffer [row] [0] = '@';
+	  		display_buffer [row] [79] = '@';
+	   }
+	  strcpy(display_buffer [1] ,
+			  "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\0");
+	  strcpy(display_buffer [23],
+			  "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\0");
+
+	  //Defining player deadzones (to bring them to the otherside of the border)
+	  if (player_location[0] < 3) {
+		  player_location[0] = 77;
+	  }
+	  else if(player_location[0] > 77){
+		  player_location[0] = 3;
+	  }
+	  if (player_location[1] < 3) {
+	  		  player_location[1] = 21;
+	  }
+	  else if(player_location[1] > 21){
+		  player_location[1] = 3;
+	  }
+
+	  //defining the 'sprite' for the player character
+	  display_buffer [player_location[1]] [player_location[0]] = '*';
+	  display_buffer [player_location[1] + 1] [player_location[0]+1] = '/';
+	  display_buffer [player_location[1] - 1] [player_location[0]-1] = '/';
+	  display_buffer [player_location[1] - 1] [player_location[0]+1] = '\\';
+	  display_buffer [player_location[1] + 1] [player_location[0]-1] = '\\';
+	  display_buffer [player_location[1]] [player_location[0]+1] = '|';
+	  display_buffer [player_location[1]] [player_location[0]-1] = '|';
+	  display_buffer [player_location[1] - 1] [player_location[0]] = '-';
+	  display_buffer [player_location[1] + 1] [player_location[0]] = '-';
+
+
+	  //Refresh the screen when the flag is set to do so
 	  if (refreshing == 1){
-		  refreshScreen(displayBuffer);
+		  refresh_screen(display_buffer);
 		  refreshing = 0;
 	  }
 
+	  //Switch case to check for player input, refresh upon key press, also moves the player location
 	  switch(keyPressed){
 	  case 'w':
 		  refreshing = 1;
-		  playerLocation[1]--;
+		  player_location[1]--;
 		  keyPressed = 0;
 		  break;
 	  case 's':
 		  refreshing = 1;
-		  playerLocation[1]++;
+		  player_location[1]++;
 		  keyPressed = 0;
 		  break;
 	  case 'a':
 		  refreshing = 1;
-		  playerLocation[0]--;
+		  player_location[0]--;
 		  keyPressed = 0;
 		  break;
 	  case 'd':
 		  refreshing = 1;
-		  playerLocation[0]++;
+		  player_location[0]++;
 		  keyPressed = 0;
 		  break;
 	  default:
@@ -69,21 +169,6 @@ int main(void)
 		  keyPressed = 0;
 		  break;
 	  }
-	  for (int row = 0; row < 24; row++){
-	  		strcpy(displayBuffer[row],"                                                                                \0");
-	    }
-
-	  displayBuffer [playerLocation[1]] [playerLocation[0]] = 'T';
-	  displayBuffer [playerLocation[1] + 1] [playerLocation[0]+1] = 'T';
-	  displayBuffer [playerLocation[1] - 1] [playerLocation[0]-1] = 'T';
-	  displayBuffer [playerLocation[1] - 1] [playerLocation[0]+1] = 'T';
-	  displayBuffer [playerLocation[1] + 1] [playerLocation[0]-1] = 'T';
-	  displayBuffer [playerLocation[1]] [playerLocation[0]+1] = 'T';
-	  displayBuffer [playerLocation[1]] [playerLocation[0]-1] = 'T';
-	  displayBuffer [playerLocation[1] - 1] [playerLocation[0]] = 'T';
-	  displayBuffer [playerLocation[1] + 1] [playerLocation[0]] = 'T';
-
-
 
 
 
@@ -91,7 +176,10 @@ int main(void)
 }
 
 
-void refreshScreen(char displayBuffer[][81]) {
+
+// Refreshes the screen, accepts a display_buffer 2D array,
+// Expects a display that is 24 rows long, and 80 characters wide
+void refresh_screen(char displayBuffer[24][81]) {
 
 	for (int row = 0; row < 24; row++){
 		LPUART_moveCursor(row, 0); // Move cursor to the beginning of the current row
